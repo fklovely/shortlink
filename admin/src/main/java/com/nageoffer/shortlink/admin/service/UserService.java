1c8fc525338ea705f2e0b940ec44641acf85fcae
fklovely <fkagoodday@163.com>
2025-10-06 14:20:00 +0800

    Initial commit

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -0,0 +1,15 @@
+package com.nageoffer.shortlink.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+
+public interface UserService extends IService<UserDO> {
+    /**
+     * 根据用户名查询用户信息
+     * @param username 用户名
+     * @return 用户返回实体
+     */
+    UserRespDTO getUserByName(String username);
+
+}
